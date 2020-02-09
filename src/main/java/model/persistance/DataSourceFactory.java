7948e9e460cd0b60e8ed7f93883ddac70c87d751
Dmytro Krutii <solosuicide133@gmail.com>
2020-02-09 15:11:11 +0200

    persistance

---
+++ src/main/java/model/persistance/DataSourceFactory.java
@@ -0,0 +1,64 @@
+package model.persistance;
+
+import com.impossibl.postgres.jdbc.PGDataSource;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+
+public class DataSourceFactory {
+
+    public static final String DB_PROPERTIES = "/database/db.properties";
+    public static final String DB_HOST = "db.host";
+    public static final String DB_NAME = "db.name";
+    public static final String DB_URL = "db.url";
+    public static final String DB_PORT = "db.port";
+    public static final String DB_USERNAME = "db.login";
+    public static final String DB_PASSWORD = "db.password";
+
+    private static final DataSourceFactory INSTANCE = new DataSourceFactory();
+
+    private static PGDataSource dataSource;
+
+    private DataSourceFactory() {
+    }
+
+    static {
+        Properties properties = new Properties();
+        try {
+            properties.load(DataSourceFactory.class.getResourceAsStream(DB_PROPERTIES));
+            PGDataSource source = new PGDataSource();
+            source.setDatabaseName(properties.getProperty(DB_NAME));
+            source.setHost(properties.getProperty(DB_HOST));
+            source.setPort(Integer.parseInt(properties.getProperty(DB_PORT)));
+            source.setUser(properties.getProperty(DB_USERNAME));
+            source.setPassword(properties.getProperty(DB_PASSWORD));
+            dataSource = source;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static PreparedStatement getPreparedStatement(String query) throws SQLException {
+        return getConnection().prepareStatement(query);
+    }
+
+    public static PreparedStatement getPreparedStatementWithReturning(String query) throws SQLException {
+        return getConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+    }
+}
